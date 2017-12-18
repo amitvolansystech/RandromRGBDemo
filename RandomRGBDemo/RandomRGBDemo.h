8567db1a66d3ea35c2630fa88071cd4938bd6b0f
amitvolansystech <amit.mishra@volansystech.com>
2017-12-18 12:14:48 +0530

    Initial commit

---
+++ RandomRGBDemo/RandomRGBDemo.h
@@ -0,0 +1,19 @@
+//
+//  RandomRGBDemo.h
+//  RandomRGBDemo
+//
+//  Created by Amit Mishra on 18/12/17.
+//  Copyright © 2017 Volansys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RandomRGBDemo.
+FOUNDATION_EXPORT double RandomRGBDemoVersionNumber;
+
+//! Project version string for RandomRGBDemo.
+FOUNDATION_EXPORT const unsigned char RandomRGBDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RandomRGBDemo/PublicHeader.h>
+
+
---
+++ RandomRGBDemo/RandomRGBOne.swift
@@ -0,0 +1,23 @@
+//
+//  RandomRGBOne.swift
+//  RandomRGBDemo
+//
+//  Created by Amit Mishra on 18/12/17.
+//  Copyright © 2017 Volansys. All rights reserved.
+//
+
+import Foundation
+public func randomRGBUIColor()->UIColor {
+    return .randomeColor()
+}
+
+extension CGFloat {
+    static func random()-> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(Int32.max)
+    }
+}
+extension UIColor {
+    static func randomeColor ()-> UIColor {
+        return UIColor(red: .random(), green: .random(), blue: .random(), alpha: 1.0)
+    }
+}
